19c218442cd524729ba7cff99535310d9473867f
Katominh <jasonandkhangminh@gmail.com>
2025-03-17 13:31:57 -0700

    Added "last updated" feature and used "last-child"

--- time.js
+++ time.js
@@ -1,5 +1,11 @@
+// Put the website's updated time here
+const update = new Date("2025-03-17");
+document.getElementById("lastUpdate").innerHTML = "<u>Last Updated:</u> " + update.toDateString();
+
+// ----------------------------------------------------------------------------------------------
 setInterval(getTime, 1000); // reload every 1s
 
 function getTime() {
-    document.getElementById("time").innerHTML = new Date();
+    const time = new Date();
+    document.getElementById("time").innerHTML = time;
 }
