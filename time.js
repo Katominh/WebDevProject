0f3850cbfea226d1de84235a013e4f640dc13e51
Katominh <jasonandkhangminh@gmail.com>
2025-03-17 20:48:27 -0700

    Hid Extra Elements and Added Animations

--- time.js
+++ time.js
@@ -3,7 +3,7 @@ const update = new Date("2025-03-17");
 document.getElementById("lastUpdate").innerHTML = "<u>Last Updated:</u> " + update.toDateString();
 
 // ----------------------------------------------------------------------------------------------
-setInterval(getTime, 1000); // reload every 1s
+setInterval(getTime, 1000); // change every 1s
 
 function getTime() {
     const time = new Date();
